9bca90aaa06895efe46cd48a02c50186da1d5e1e
Tim Davis <timdavish@gmail.com>
2018-06-23 10:45:59 -0700

    create client/server axios instances

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,25 @@
 import 'babel-polyfill'
 import express from 'express'
+import proxy from 'express-http-proxy'
 import {matchRoutes} from 'react-router-config'
 import Routes from './client/Routes'
 import createStore from './helpers/createStore'
 import renderer from './helpers/renderer'
 
 const app = express()
+const proxyUrl = 'http://react-ssr-api.herokuapp.com'
+const proxyOptions = {
+  proxyReqOptDecorator (options) {
+    options.headers['x-forwarded-host'] = 'localhost:3000'
+    return options
+  }
+}
 
+app.use('/api', proxy(proxyUrl, proxyOptions))
 app.use(express.static('public'))
 
 app.get('*', async (req, res) => {
-  const store = createStore()
+  const store = createStore(req)
 
   // Load all necessary component data
   await Promise.all(
